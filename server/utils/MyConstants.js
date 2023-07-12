dff3ada07c3137563f7c6fccec168cc3ae8a8cc4
NhatThanhRea <128576189+NhatThanhRea@users.noreply.github.com>
2023-07-12 08:55:11 +0700

    3 project

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+  DB_SERVER: 'cluster0.ze7adqt.mongodb.net',
+  DB_USER: 'lamnhatthanh6868',
+  DB_PASS: '01296294344a',
+  DB_DATABASE: 'shoppingonline',
+  EMAIL_USER: 'lamnhatthanh6868@gmail.com', // Microsoft mail service
+  EMAIL_PASS: 'npphjjwnbzwrfxza',
+  JWT_SECRET: '123456',
+  JWT_EXPIRES: '60000000', // in milliseconds
+};
+module.exports = MyConstants;
